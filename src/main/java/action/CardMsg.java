d800232063ff1d3ac2810f172d5851cf5dd9ad4c
shrek1994 <maciej.wozny41@gmail.com>
2015-11-28 15:55:17 +0100

    Add Card message

---
+++ src/main/java/action/CardMsg.java
@@ -0,0 +1,19 @@
+package action;
+
+import cards.Card;
+
+/**
+ * Created by maciek on 28.11.15.
+ */
+public class CardMsg {
+    private Card card;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public CardMsg(Card card) {
+
+        this.card = card;
+    }
+}
