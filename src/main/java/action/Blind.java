36e089a934ae8ce0b85c00a3def47bcb3d9c42e8
shrek1994 <maciej.wozny41@gmail.com>
2015-11-28 15:53:45 +0100

    Add Blind message

---
+++ src/main/java/action/Blind.java
@@ -0,0 +1,17 @@
+package action;
+
+/**
+ * Created by maciek on 28.11.15.
+ */
+public class Blind {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public Blind(int value) {
+
+        this.value = value;
+    }
+}
